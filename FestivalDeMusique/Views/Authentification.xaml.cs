241b5944e6172bcec07790e11ac9b4af1548cb9f
MorganDara <morganslba4@gmail.com>
2021-04-22 13:20:04 +0200

    Redesigning de la page d'Authentification

---
+++ FestivalDeMusique/Views/Authentification.xaml.cs
@@ -0,0 +1,86 @@
+﻿using FestivalAPI.Models;
+using FestivalDeMusique.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FestivalDeMusique.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour Authentification.xaml
+    /// </summary>
+    public partial class Authentification : Window
+    {
+        public Authentification()
+        {
+            InitializeComponent();
+        }
+
+        private void Fermer_Fenetre(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void DragFrame(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void LoginFunction(object sender, RoutedEventArgs e)
+        {
+            string email = textBoxEmail.Text;
+            string pass = passwordTextBox.Text;
+
+            if (email.Trim().Length == 0 || pass.Trim().Length == 0)
+            {
+                MessageBox.Show("Veuillez remplir tous les champs");
+            }
+            else
+            {
+                //pass = Hash(pass);
+                if (CheckCredentials(email, pass))
+                {
+                    Hide();
+                    MenuGestionnaire menu = new MenuGestionnaire();
+                    menu.ShowDialog();
+                    Show();
+                }
+                else
+                {
+                    MessageBox.Show("E-mail ou mot de passe incorrect.");
+                }
+            }
+        }
+
+        private bool CheckCredentials(string email, string pass)
+        {
+            return true;
+            Gimi gimi = API.API.Instance.GetGimi(email, pass).Result;
+            try
+            {
+                return email == gimi.Login && pass == gimi.Pwd;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private string Hash(String mdp)
+        {
+            var bytes = new UTF8Encoding().GetBytes(mdp);
+            var hashBytes = System.Security.Cryptography.MD5.Create().ComputeHash(bytes);
+            return Convert.ToBase64String(hashBytes);
+        }
+    }
+}
