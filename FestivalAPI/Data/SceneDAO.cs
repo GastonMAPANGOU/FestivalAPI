337030260433e51700e7bbd27b7f370975b5db09
Lanvin26 <lanvinnguetcho@gmail.com>
2021-05-09 17:41:35 +0200

    Modification des classes

--- FestivalAPI/Data/SceneDAO.cs
+++ FestivalAPI/Data/SceneDAO.cs
@@ -74,6 +74,38 @@ namespace FestivalAPI.Data
             }
         }
 
+        public List<Scene> Display_Scene()
+        {
+            List<Scene> list_Scene = new List<Scene>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "Select * from Scene Order By Nom ASC ";
+                SqlCommand command = new SqlCommand(sqlQuery, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Scene scene = new Scene();
+
+                        scene.IdS = reader.GetInt32(0);
+                        scene.Nom = reader.GetString(1);
+                        scene.Adresse = reader.GetString(2);
+                        scene.Capacite = reader.GetInt32(3);
+                        scene.Accessibilite = reader.GetString(2);
+                        scene.LieuId = reader.GetInt32(5);
+
+                        list_Scene.Add(scene);
+                    }
+                }
+
+                return list_Scene;
+            }
+        }
+
         public List<Scene>List_Scene(List<Festival_Artiste>list_Festival_Artiste)
         {
 
---
+++ FestivalAPI/Models/Commentaire.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FestivalAPI.Models
+{
+    public class Commentaire
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("FK_Festivalier")]
+        public int FestivalierId { get; set; }
+        public string Comment { get; set; }
+    }
+}
