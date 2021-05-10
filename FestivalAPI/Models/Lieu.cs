3b4962cf01bc4b05589177df3bf7d8b15abf97f5
GastonMNGC <gaston.mapangou@groupe-esigelec.org>
2021-05-10 11:26:44 +0200

    les modif de Gaetan

--- FestivalAPI/Models/Lieu.cs
+++ FestivalAPI/Models/Lieu.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,8 @@ namespace FestivalAPI.Models
         [Key]
         public int IdL { get; set; }
         public string Commune { get; set; }
+        [ForeignKey("FK_Pays")]
+        public int PaysId { get; set;}
         public ICollection<Festival> Festivals { get; set; }
         public ICollection<Scene> Scenes { get; set; }
         public ICollection<Hebergement> Hebergements { get; set; }
