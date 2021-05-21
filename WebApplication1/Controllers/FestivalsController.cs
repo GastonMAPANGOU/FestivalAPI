670b2d088753cdc5000fe72b80681a452d918085
GastonMNGC <gaston.mapangou@groupe-esigelec.org>
2021-05-21 22:56:58 +0200

    ajouts

--- WebApplication1/Controllers/FestivalsController.cs
+++ WebApplication1/Controllers/FestivalsController.cs
@@ -446,11 +446,14 @@ namespace WebApplication1.Controllers
 
         public IActionResult AjoutScene(int? id)
         {
-            if (id == null)
+            if(id==null)
             {
-                return null;
+                Organisateur organisateur = API.Instance.GetOrganisateurAsync((int)HttpContext.Session.GetInt32("ido")).Result;
+                Festival festival = API.Instance.GetFestivalAsync(organisateur.FestivalId).Result;
+                return View(festival);
             }
-            return View(API.Instance.GetFestivalAsync(id).Result);
+            return View();
+
         }
 
 
@@ -534,11 +537,9 @@ namespace WebApplication1.Controllers
 
         public IActionResult AjoutHebergement(int? id)
         {
-            if (id == null)
-            {
-                return null;
-            }
-            return View(API.Instance.GetFestivalAsync(id).Result);
+            Organisateur organisateur = API.Instance.GetOrganisateurAsync((int)HttpContext.Session.GetInt32("ido")).Result;
+            Festival festival = API.Instance.GetFestivalAsync(organisateur.FestivalId).Result;
+            return View(festival);
         }
 
 
@@ -618,6 +619,88 @@ namespace WebApplication1.Controllers
         {
             var URI = API.Instance.SupprHebergementAsync(id);
             return Redirect("/Festivals/Festivaliers");
+        }
+
+        // GET: Artiste/Create
+        public IActionResult AjoutArtiste()
+        {
+            Organisateur organisateur = API.Instance.GetOrganisateurAsync((int)HttpContext.Session.GetInt32("ido")).Result;
+            Festival festival = API.Instance.GetFestivalAsync(organisateur.FestivalId).Result;
+            return View(festival);
+        }
+
+
+
+        // POST: Artiste/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AjoutArtiste(Artiste artiste, IFormFile file, IFormFile file2, [FromServices] IHostingEnvironment hostingEnvironment)
+        {
+            //taf de la photo
+            int taillemax = 2097152;
+
+            List<String> extensionsvalides = new List<String>();
+            List<String> strcut = new List<String>();
+            extensionsvalides.Add(".jpg");
+            extensionsvalides.Add(".jpeg");
+            extensionsvalides.Add(".gif");
+            extensionsvalides.Add(".png");
+            extensionsvalides.Add(".jfif");
+            string fileNamtre = file.FileName;
+
+            string fileName = "img/artistes/photos" + artiste.Nom;
+            string extension = Path.GetExtension(file.FileName);
+            string chemin = fileName + extension.ToLower();
+            //taf de l'extrait musical
+            List<String> extensionsvalides2 = new List<String>();
+            List<String> strcut2 = new List<String>();
+            extensionsvalides2.Add(".mp3");
+            
+            string fileNamtre2 = file2.FileName;
+
+            string fileName2 = "img/artistes/extraits" + artiste.Nom;
+            string extension2 = Path.GetExtension(file2.FileName);
+            string chemin2 = fileName2 + extension2.ToLower();
+            if (file.Length < taillemax && extensionsvalides.Contains(extension) && extensionsvalides2.Contains(extension2))
+            {
+                using (FileStream fileStream = System.IO.File.Create("wwwroot/" + chemin))
+                {
+                    file.CopyTo(fileStream);
+                    fileStream.Flush();
+                }
+                using (FileStream fileStream = System.IO.File.Create("wwwroot/" + chemin2))
+                {
+                    file.CopyTo(fileStream);
+                    fileStream.Flush();
+                }
+                artiste.Photo = chemin;
+                artiste.Extrait = chemin2;
+
+
+
+                int drapeau = 0;
+                IEnumerable<Artiste> Artistes = API.Instance.GetArtistesAsync().Result;
+                foreach (var item in Artistes)
+                {
+                    if (item.Nom == artiste.Nom)
+                    {
+                        drapeau++;
+                    }
+                }
+
+                if (ModelState.IsValid && drapeau == 0)
+                {
+                    var URI = API.Instance.AjoutArtisteAsync(artiste);
+                    return RedirectToAction(nameof(Index));
+                }
+                else if (drapeau != 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(artiste);
         }
 
         public IActionResult Festivaliers(int? id)
