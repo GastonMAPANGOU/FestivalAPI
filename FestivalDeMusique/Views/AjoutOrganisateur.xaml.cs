037aa445c957ad7aac4672ec38c7baeafb7bee03
MorganDara <morganslba4@gmail.com>
2021-05-26 11:40:40 +0200

    Finalisation de la gestion des organisateurs dans le client lourd

--- FestivalDeMusique/Views/AjoutOrganisateur.xaml.cs
+++ FestivalDeMusique/Views/AjoutOrganisateur.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using FestivalAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +21,53 @@ namespace FestivalDeMusique.Views
     /// </summary>
     public partial class AjoutOrganisateur : Window
     {
-        public AjoutOrganisateur()
+        private readonly Festival festival;
+        public AjoutOrganisateur(Festival festival)
         {
             InitializeComponent();
+            this.festival = festival;
+            FestivalTextBlock.Text = festival.Nom;
+        }
+
+        private void Retour_ButtonClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private async void Ajouter_ButtonClick(object sender, RoutedEventArgs e)
+        {
+            string nom = nomTextBox.Text;
+            string prenom = prenomTextBox.Text;
+            string login = emailTextBox.Text;
+            string pwd = motDePasseTextBox.Text;
+            bool validation = validationCheckBox.IsChecked.Value;
+            if(nom.Trim().Length == 0 || prenom.Trim().Length == 0 || login.Trim().Length == 0 || pwd.Trim().Length == 0)
+            {
+                MessageBox.Show("Veuillez remplir tous les champs textuels");
+            }
+            else
+            {
+                Organisateur organisateur = new Organisateur()
+                {
+                    Nom = nom, Prenom = prenom, FestivalId = festival.IdF, InscriptionAccepted = validation, 
+                    Login = login, Pwd = pwd
+                };
+
+                HttpResponseMessage response = await API.API.Instance.AjoutOrganisateurAsync(organisateur);
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Nouvel organisateur ajouté avec succès");
+                    nomTextBox.Text = "";
+                    prenomTextBox.Text = "";
+                    emailTextBox.Text = "";
+                    motDePasseTextBox.Text = "";
+                    validationCheckBox.IsChecked = false;
+                }
+                else
+                {
+                    MessageBox.Show("Erreur lors de l'ajout de l'organisateur");
+                }
+            }
         }
     }
 }
--- FestivalDeMusique/Views/ModifierOrganisateur.xaml.cs
+++ FestivalDeMusique/Views/ModifierOrganisateur.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using FestivalAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +21,59 @@ namespace FestivalDeMusique.Views
     /// </summary>
     public partial class ModifierOrganisateur : Window
     {
-        public ModifierOrganisateur()
+        private readonly Festival festival;
+        private readonly Organisateur organisateur;
+        public ModifierOrganisateur(Organisateur organisateur)
         {
             InitializeComponent();
+            this.organisateur = organisateur;
+            this.festival = API.API.Instance.GetFestivalAsync(organisateur.FestivalId).Result;
+            ChargerOrganisateur();
+        }
+
+        private void Retour_ButtonClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private async void Valider_ButtonClick(object sender, RoutedEventArgs e)
+        {
+            string nom = nomTextBox.Text;
+            string prenom = prenomTextBox.Text;
+            string login = emailTextBox.Text;
+            string pwd = motDePasseTextBox.Text;
+            bool validation = validationCheckBox.IsChecked.Value;
+            if (nom.Trim().Length == 0 || prenom.Trim().Length == 0 || login.Trim().Length == 0 || pwd.Trim().Length == 0)
+            {
+                MessageBox.Show("Veuillez remplir tous les champs textuels");
+            }
+            else
+            {
+                Organisateur org = organisateur;
+                org.Nom = nom; org.Prenom = prenom; org.Login = login; org.Pwd = pwd;
+                org.InscriptionAccepted = validation;
+
+                HttpResponseMessage response = await API.API.Instance.ModifOrganisateurAsync(org);
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Organisateur modifié avec succès");
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Erreur lors de la modification de l'organisateur");
+                }
+            }
+        }
+
+        private void ChargerOrganisateur()
+        {
+            nomTextBox.Text = organisateur.Nom;
+            prenomTextBox.Text = organisateur.Prenom;
+            emailTextBox.Text = organisateur.Login;
+            motDePasseTextBox.Text = organisateur.Pwd;
+            validationCheckBox.IsChecked = organisateur.InscriptionAccepted;
+            FestivalTextBlock.Text = festival.Nom;
         }
     }
 }
